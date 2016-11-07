9a95e42fea0d88cd42ad453d4c835b24580762b0
germanfelixx <germannfelixx@gmail.com>
2016-11-06 23:41:04 -0600

    commit 2
    terminado 1 y 2

--- app.component.js
+++ app.component.js
@@ -11,11 +11,40 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var AppComponent = (function () {
     function AppComponent() {
+        this.title = 'Ultra racing';
+        this.carParts = [{
+                "id": 1,
+                "name": "super ludwing",
+                "description": "these tires are the very best",
+                "inStock": 5,
+                "price": 5.90
+            },
+            {
+                "id": 2,
+                "name": "super daniela",
+                "description": "these tires are the very best",
+                "inStock": 2,
+                "price": 10.90
+            }, {
+                "id": 3,
+                "name": "super german",
+                "description": "these tires are the very best",
+                "inStock": 0,
+                "price": 8.90
+            }];
     }
+    AppComponent.prototype.totalCarParts = function () {
+        var sum = 0;
+        for (var _i = 0, _a = this.carParts; _i < _a.length; _i++) {
+            var carPart = _a[_i];
+            sum += carPart.inStock;
+        }
+        return sum;
+    };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>My First Angular sApp</h1>'
+            template: "<h1>{{title}}</h1>\n  <p>There are {{totalCarParts()}} total parts in stock.</p>\n  <ul>\n    <li *ngFor=\"let carPart of carParts\">\n  <h2>{{carPart.name}}</h2>\n <p> {{carPart.description}}<p>\n <p>{{carPart.price | currency:'EUR':true}}</p>\n  <p *ngIf=\"carPart.inStock>0\">{{carPart.inStock}} in Stock</p>\n  <p *ngIf=\"carPart.inStock===0\">Out of Stock</p>\n </li>\n </ul>\n  "
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- app.component.ts
+++ app.component.ts
@@ -1,6 +1,46 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular sApp</h1>'
+  template: `<h1>{{title}}</h1>
+  <p>There are {{totalCarParts()}} total parts in stock.</p>
+  <ul>
+    <li *ngFor="let carPart of carParts">
+  <h2>{{carPart.name}}</h2>
+ <p> {{carPart.description}}<p>
+ <p>{{carPart.price | currency:'EUR':true}}</p>
+  <p *ngIf="carPart.inStock>0">{{carPart.inStock}} in Stock</p>
+  <p *ngIf="carPart.inStock===0">Out of Stock</p>
+ </li>
+ </ul>
+  `
 })
-export class AppComponent { }
+export class AppComponent { 
+  title='Ultra racing';
+  carParts=[{
+"id":1,
+"name":"super ludwing",
+"description":"these tires are the very best",
+"inStock":5,
+"price":5.90
+  },
+  {
+"id":2,
+"name":"super daniela",
+"description":"these tires are the very best",
+"inStock":2,
+"price":10.90
+  },{
+  "id":3,
+"name":"super german",
+"description":"these tires are the very best",
+"inStock":0,
+"price":8.90
+  }];
+  totalCarParts(){
+    let sum = 0;
+    for(let carPart of this.carParts){
+sum+=carPart.inStock;
+    }
+    return sum;
+  }
+}
